44bf7594b47035bfa00620309e5df233002394f1
vijendra8475 <vijjubca@gmail.com>
2026-04-18 12:38:59 +0530

    [ Reset Everythings ]

--- src/routes/webhook.routes.js
+++
@@ -1,8 +0,0 @@
-import express from "express";
-import { handleWebhook } from "../controllers/webhook.controller.js";
-
-const router = express.Router();
-
-router.post("/", handleWebhook);
-
-export default router;
